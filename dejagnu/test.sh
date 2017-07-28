a4ca01ca3171a740de15dca5c5e1ceaf4edf9841
Martin Aberg <martin@fripost.org>
2017-07-28 17:56:05 +0200

    test: Archive GCC test logs

--- dejagnu/test.sh
+++ dejagnu/test.sh
@@ -2,11 +2,19 @@
 set -o errexit
 set -o nounset
 
+HOSTBITS=`uname -m | grep -e 32 -e 64 -o`
+HOSTOS=`uname -s | tr '[:upper:]' '[:lower:]'`
+HOSTDESC=$HOSTOS$HOSTBITS
+
 # Find GNU Make
 if type gmake > /dev/null 2>&1 ; then
         gmake=gmake
 else
         gmake=make
+fi
+if ! type vamos > /dev/null 2>&1 ; then
+        echo "ERROR: 'vamos' not in PATH"
+        exit 1
 fi
 
 SCRIPTDIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
@@ -14,9 +22,18 @@ ASDFTOP="$( readlink -f "${SCRIPTDIR}/..")"
 
 pushd $ASDFTOP/build-gcc/gcc
 DEJAGNU=$ASDFTOP/dejagnu/dejagnurc $gmake -j4 check-gcc//vamos/{-m68000,-m68020/-msoft-float}
-popd
+
+d=$(./xgcc --version|grep -o "(asdf-.\+)" | tr -d "()")
+dd="$SCRIPTDIR/$d-$HOSTDESC"
 
 echo "----"
-echo "Test log files:"
-find $ASDFTOP/build-gcc/gcc/testsuite* -name gcc.sum -o -name gcc.log
+echo "Copying log files to $dd"
+
+mkdir -p $dd
+cp -v testsuite.vamos.-m68000/gcc/gcc.sum $dd/gcc-000.sum
+cp -v testsuite.vamos.-m68000/gcc/gcc.log $dd/gcc-000.log
+cp -v testsuite.vamos.-m68020.-msoft-float/gcc/gcc.sum $dd/gcc-020soft.sum
+cp -v testsuite.vamos.-m68020.-msoft-float/gcc/gcc.log $dd/gcc-020soft.log
+
+popd
 
